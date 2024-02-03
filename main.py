e1824caffb39b9897fc44ab55c3b25aa306abbb8
Kydunc04 <kyledu8s@gmail.com>
2024-02-03 10:50:33 +0000

    Groups

--- main.py
+++ main.py
@@ -4,5 +4,18 @@ print('Hello, Kyle')
 
 celsius = 38.4
 fahrenheit = (1.8*celsius)+32
-print (fahrenheit,celsius)
+print(fahrenheit, celsius)
 
+TimesBatted = 1014
+NotOut = 162
+BatAv = TimesBatted-NotOut
+print(BatAv)
+
+St1, St2, St3 = 113, 175, 12
+Grp1, Grp2, Grp3 = (St1/24, St2/24, St3/24)
+r1 = round(Grp1)
+r2 = round(Grp2)
+r3 = round(Grp3)
+f2 = (St1 % 24, St2 % 24, St3 % 24)
+print(r1, r2, r3)
+print(f2)
